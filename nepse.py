97d193bb8bd924512800d6de6610d5e2ef92b736
SWASTIK SHRESTHA <SWASTIKS@nibl.com.np>
2021-09-30 10:19:14 +0545

    updated

--- nepse.py
+++ nepse.py
@@ -1,7 +1,7 @@
 from typing import Any, cast
 from bs4 import BeautifulSoup
 from selenium import webdriver
-import time 
+import time
 from selenium.webdriver.chrome.options import Options
 import pandas as pd
 import configparser
@@ -19,6 +19,7 @@ banks_str = _config.get("DEFAULT", 'banks')
 banks = banks_str.split(",")
 
 data_list = list()
+
 
 def main():
     for bankid in banks:
@@ -47,9 +48,10 @@ def main():
         if(len(parsed_table) > 0):
             table = parsed_table[0]
             rows = table.find_all('tr')
-            bankname = soup.find(class_="company__title--details").find("h1").text
-            parser(rows=rows,bankname=bankname)
-    
+            bankname = soup.find(
+                class_="company__title--details").find("h1").text
+            parser(rows=rows, bankname=bankname)
+
     export(data_list=data_list)
     #         ltp_data = []
     #         index = 0
@@ -67,19 +69,18 @@ def main():
     #                 ",", "")), float(ltp_data[2].replace("(", "").replace(")", "").replace("%", "")))
 
 
-
 def parser(rows, bankname):
     ltp_data = []
     index = 0
     ltp_data.append(bankname)
     for tr in rows:
-        if index == 3 or index==0:
+        if index == 3 or index == 0:
             for span in tr.find_all("span"):
                 ltp_data.append(span.text.strip())
         else:
             for th in tr.find_all("td"):
                 ltp_data.append(th.text.strip())
-            
+
         index = index + 1
 
     # ltp,change price , change %
@@ -95,8 +96,8 @@ def export(data_list):
         'As of',
         'Instrument Type',
         'Listing Date',
-        'Last Traded Price', 
-        'Change Price', 
+        'Last Traded Price',
+        'Change Price',
         'Change Percentage',
         'Total Traded Quantity',
         'Total Trades',
@@ -109,12 +110,13 @@ def export(data_list):
         'Total Paid up Value',
         'Market Capitalization',
         'Notes'
-        ])
+    ])
     df_bs.set_index('Bank Name', inplace=True)
     print(df_bs.head())
 
     # Exporting the data into csv
-    df_bs.to_csv('beautifulsoup-' + datetime.now().strftime("%d-%m-%Y %H-%M-%S") + '.csv')
+    df_bs.to_csv('beautifulsoup-' +
+                 datetime.now().strftime("%d-%m-%Y %H-%M-%S") + '.csv')
 
 
 if __name__ == '__main__':
